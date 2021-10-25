113b0ccc8011e68d82bfee9a144083a761870933
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-10-25 11:13:59 +0900

    fix: modify tweet message

--- check_ranking_data.py
+++ check_ranking_data.py
@@ -64,7 +64,8 @@ def checkUploadData(datas):
 
         message = "[info][title]【事前確認用】本日のPeoPle'sランキングツイート[/title]"
         message += f"ꉂꉂ📢PeoPle's 検索順位速報✨\n\n"
-        message += f"◎計測地域：新宿🏙\n\n"
+        message += f"◎計測地域：新宿🏙\n"
+        message += f"ーーー\n\n"
 
         for data in datas:
             rdate = datetime.datetime.strptime(data[9], '%b %d, %Y').strftime('%Y/%m/%d')
